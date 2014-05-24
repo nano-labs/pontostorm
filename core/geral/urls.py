0f4f689599e32cabc3cc9170f10034fe11f057c3
Leonardo Cabral <leonardo.cabral@stormsec.com.br>
2014-05-24 08:51:31 -0300

    Corrigindo url do grappelli.

--- core/geral/urls.py
+++ core/geral/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^grappelli/', include('grappelli.urls')),
 )
 
 if settings.DEBUG:
