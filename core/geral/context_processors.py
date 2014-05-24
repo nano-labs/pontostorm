d3a31ec1725326e50705873e81af67440e357c85
Leonardo Cabral <leonardo.cabral@stormsec.com.br>
2014-05-24 08:22:05 -0300

    Estrutura base do django 1.6.5

--- core/geral/context_processors.py
+++ core/geral/context_processors.py
@@ -1,8 +1,7 @@
 # -*- encoding: utf-8 -*-
 
-
 def context(request):
     '''
     Define um contexto de variaveis acessíveis em todas as views.
     '''
-    return {'generic_viarable': 'variavel do context processor'}
+    return {}
--- core/geral/urls.py
+++ core/geral/urls.py
@@ -1,23 +1,20 @@
-from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^generic_app/', include('generic_app.urls')),
+    url(r'^$', 'geral.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
-    # url(r'^$', 'geral.views.home', name='home'),
-    # url(r'^core/', include('core.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
 if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += patterns('django.views.static',
-        (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    )
+                            (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT,
+                                                              'show_indexes': True}),)
