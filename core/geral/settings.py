8678d18db6e8c75983ada61a362f2bb402e4be06
Leonardo Cabral <leonardo.cabral@stormsec.com.br>
2014-10-11 01:34:56 -0300

    Compatibilizando estrutura com django 1.7.

--- core/geral/settings.py
+++ core/geral/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -45,8 +46,6 @@ INSTALLED_APPS = (
 
     # 'grappelli.dashboard',
     'grappelli',
-    'django.contrib.admin',
-    'south',
 
     'geral',
 )
@@ -56,6 +55,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
